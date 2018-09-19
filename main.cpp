a231dd855dc8a2eecc77775a043678c4d705fde2
jaguirreayala <jaguirre-ayala@mail.csuchico.edu>
2018-09-19 16:45:18 -0700

    Asks for users to input their first name and last name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: Juan Aguirre-Ayala
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,13 @@ using namespace std;
 int main()
 {
   //declare variables
-
+    string first;
+    string last;
   //get user input
-
+    cout << "Please enter your first name\n>";
+    cin >> first;
+    cout << "Now please enter your last name\n>";
+    cin >> last;
   //tell fortune
 
   return 0;
