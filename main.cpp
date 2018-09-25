30d0aeef9dd60a99539e1d12451d8e44225e317f
jaguirreayala <jaguirre-ayala@mail.csuchico.edu>
2018-09-24 17:43:01 -0700

    Identifies user's first letter of first name and last letter of last name and returns a fortune based on if they are a vowel or not

--- main.cpp
+++ main.cpp
@@ -9,16 +9,36 @@ int main()
   //declare variables
     string first;
     string last;
+    int index;
   //get user input
     cout << "Please enter your first name\n>";
     cin >> first;
     cout << "Now please enter your last name\n>";
     cin >> last;
 
+    index = last.length() - 1;
+    
     string fi = first;
     string li = last;
     cout << "Welcome, " << fi[0] << "." << li[0] << "., here is your fortune...\n";
   //tell fortune
-    cout << "Your lucky number is " << fi.length() << endl;
-  return 0;
+    cout << "your lucky number is " << fi.length() << endl;
+    if(first[0] == 'a' || first[0] == 'e' || first[0] == 'i' || first[0] == 'o' || first[0] == 'u' || 
+       first[0] == 'A' || first[0] == 'E' || first[0] == 'I' || first[0] == 'O' || first[0] == 'U') 
+    {
+        cout << "you are destined to be famous!\n";
+    }
+    else
+    {
+        cout << "you should keep a low profile.\n";
+    }
+
+    if(last[index] == 'a' || last[index] == 'e' || last[index] == 'i' || last[index] == 'o' || last[index] == 'u')
+    {
+        cout << "you have already met your true love.\n";
+    }
+
+    cout << "have a good day!\n";
+    
+    return 0;
 }
